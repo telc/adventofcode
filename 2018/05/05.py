8227b170a1975499b13508388da126cfd8026e17
Chris Carlmar <chris@bridgetech.tv>
2018-12-05 15:32:58 +0100

    2018: Optimize solution for day 5 part 2
    Reusing the colapsed string makes the later calls to part1() much faster

--- 2018/05/05.py
+++ 2018/05/05.py
@@ -15,17 +15,18 @@ def part1(input_):
         i = 0
     else:
       i += 1
-  return len(input_)
+  return input_
 
-def part2(input_):
+def solve(input_):
+  p1 = part1(input_)
   best_len = 99999999
   for c in xrange(ord('a'), ord('z')+1):
-    tmp = input_.replace(chr(c), '')
+    tmp = p1.replace(chr(c), '')
     tmp = tmp.replace(chr(c-0x20), '')
-    tmp_len = part1(tmp)
+    tmp_len = len(part1(tmp))
     if tmp_len < best_len:
       best_len = tmp_len
-  return best_len
+  return len(p1), best_len
 
 if __name__ == '__main__':
-  print part1(input_), part2(input_)
+  print solve(input_)
