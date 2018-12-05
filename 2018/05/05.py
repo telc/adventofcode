0154594f7d4ae231b43bed5c12dcb642c25d7f03
Chris Carlmar <chris@bridgetech.tv>
2018-12-05 09:40:32 +0100

    2018: Add solution for day 5 (part 2)

--- 2018/05/05.py
+++ 2018/05/05.py
@@ -11,12 +11,21 @@ def part1(input_):
     if abs(ord(input_[i]) - ord(input_[i+1])) == 0x20:
       input_ = input_[:i] + input_[i+2:]
       i -= 1
+      if i < 0:
+        i = 0
     else:
       i += 1
   return len(input_)
 
 def part2(input_):
-  return 0
+  best_len = 99999999
+  for c in xrange(ord('a'), ord('z')+1):
+    tmp = input_.replace(chr(c), '')
+    tmp = tmp.replace(chr(c-0x20), '')
+    tmp_len = part1(tmp)
+    if tmp_len < best_len:
+      best_len = tmp_len
+  return best_len
 
 if __name__ == '__main__':
   print part1(input_), part2(input_)
