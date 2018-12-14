4330a0d5db535636b98d24bea9374fe97d273bd1
Chris Carlmar <chris@bridgetech.tv>
2018-12-14 10:11:43 +0100

    2018: Speed up solution for day 14

--- 2018/14/14.py
+++ 2018/14/14.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python2.7
 
 def solve(input_):
-  recipes = [3, 7]
+  recipes = '37'
   p1 = 0
   p2 = 1
-  part2 = map(int, list(str(input_)))
-  while recipes[-len(part2):] != part2 and recipes[-len(part2)-1:-1] != part2:
-    new = recipes[p1] + recipes[p2]
-    recipes += map(int, list(str(new)))
-    p1 = (p1 + 1 + recipes[p1]) % len(recipes)
-    p2 = (p2 + 1 + recipes[p2]) % len(recipes)
-  return ''.join(map(str, recipes[input_:input_+10])), len(recipes) - len(part2) - (0 if recipes[-len(part2):] == part2 else 1)
-
+  part1 = int(input_)
+  l = len(input_)
+  while recipes[-l:] != input_ and recipes[-l-1:-1] != input_:
+    new = ord(recipes[p1]) + ord(recipes[p2]) - 96
+    recipes += str(new)
+    p1 = (p1 + 1 + ord(recipes[p1])-48) % len(recipes)
+    p2 = (p2 + 1 + ord(recipes[p2])-48) % len(recipes)
+  return recipes[part1:part1+10], len(recipes) - l - (0 if recipes[-l:] == input_ else 1)
 
 if __name__ == '__main__':
-  print solve(360781)
+  print solve("360781")
