c22bdb694f3f9ec10635df26a91c39f5c659f657
Chris Carlmar <chris@bridgetech.tv>
2018-12-18 10:33:02 +0100

    2018: Speed up solution for day 18
    Even tho OrderedDefaultDict was "elegant", its dead slow, just use
    list of strings...

--- 2018/18/18.py
+++ 2018/18/18.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python2.7
 
-from collections import OrderedDict, defaultdict
-from operator import itemgetter, mul
-
-class OrderedDefaultDict(OrderedDict, defaultdict):
-  def __init__(self, default_factory=None, *args, **kwargs):
-    super(OrderedDefaultDict, self).__init__(*args, **kwargs)
-    self.default_factory = default_factory
+from operator import  mul
+from itertools import chain
 
 with open('input', 'r') as f:
   input_ = f.read().splitlines()
@@ -22,53 +17,40 @@ with open('input', 'r') as f:
 # |.||||..|.
 # ...#.|..|.""".splitlines()
 
-def printArea(area):
-  rowlen = max(area)[0]+1
-  count = 0
-  for a in area.values():
-    print a,
-    count += 1
-    if count % rowlen == 0:
-      print
-
 MINUTES = 1000000000
 
 def solve(input_):
-  area = [[l for l in line] for line in input_]
-  area = OrderedDefaultDict(lambda: '')
-  for y in xrange(len(input_)):
-    for x in xrange(len(input_[y])):
-      area[(x,y)] = input_[y][x]
+  area = input_
   minute = 0
   hashes = []
   jumped = False
   while minute < MINUTES:
     minute += 1
-    new_area = OrderedDefaultDict(lambda: '')
-    for y in xrange(len(input_)):
-      for x in xrange(len(input_[y])):
-        num_open = 0
+    new_area = []
+    for y in xrange(len(area)):
+      for x in xrange(len(area[y])):
         num_trees = 0
         num_yards = 0
         for i in xrange(-1, 2):
           for j in xrange(-1, 2):
-            if not (i == 0 and j == 0):
-              if area[(x+i,y+j)] == '.': num_open += 1
-              if area[(x+i,y+j)] == '|': num_trees += 1
-              if area[(x+i,y+j)] == '#': num_yards += 1
-        if area[(x,y)] == '.' and num_trees >= 3:
-          new_area[(x,y)] = '|'
-        elif area[(x,y)] == '|' and num_yards >= 3:
-          new_area[(x,y)] = '#'
-        elif area[(x,y)] == '#' and (num_yards < 1 or num_trees < 1):
-          new_area[(x,y)] = '.'
+            if 0 <= y+i < len(area) and 0 <= x+j < len(area[y+i]) and not (i == 0 and j == 0):
+              if area[y+i][x+j] == '|': num_trees += 1
+              if area[y+i][x+j] == '#': num_yards += 1
+        if y == len(new_area):
+          new_area.append('')
+        if area[y][x] == '.' and num_trees >= 3:
+          new_area[y] += '|'
+        elif area[y][x] == '|' and num_yards >= 3:
+          new_area[y] += '#'
+        elif area[y][x] == '#' and (num_yards < 1 or num_trees < 1):
+          new_area[y] += '.'
         else:
-          new_area[(x,y)] = area[(x,y)]
+          new_area[y] += area[y][x]
     area = new_area
-    sums = mul(*map(sum, zip(*map(lambda x: (1,0) if x == '|' else (0,1) if x == '#' else (0,0), area.values()))))
+    sums = mul(*map(sum, zip(*map(lambda x: (1,0) if x == '|' else (0,1) if x == '#' else (0,0), list(chain(*area))))))
     if minute == 10:
       part1 = sums
-    h = ''.join(area.values())
+    h = ''.join(area)
     if not jumped and h in hashes:
       loop_size = len(hashes) - hashes.index(h)
       minute += ((MINUTES - minute) / loop_size) * loop_size
